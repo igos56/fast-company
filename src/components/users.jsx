55e83781c08078dbb42fdefe5030626fccbb468c
Igor Shtyrlyaev <igosrus56@gmail.com>
2022-11-16 15:04:23 +0500

    added table display depending on users array length

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -62,31 +62,33 @@ const Users = () => {
     return(
         <>
             {renderInfoBadge()}
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
-                        <th scope="col">Профессия</th>
-                        <th scope="col">Встретился, раз</th>
-                        <th scope="col" colSpan={2}>Оценка</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(user => {
-                        return (
-                            <tr key={user._id}>
-                                <td>{user.name}</td>
-                                <td>{renderQualities(user.qualities)}</td>
-                                <td>{user.profession.name}</td>
-                                <td>{user.completedMeetings}</td>
-                                <td>{getFormatRate(user)}</td>
-                                <td><button className="btn btn-outline-danger" onClick={() => handleDelete(user._id)}>delete</button></td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            {users.length > 0 &&
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col" colSpan={2}>Оценка</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(user => {
+                            return (
+                                <tr key={user._id}>
+                                    <td>{user.name}</td>
+                                    <td>{renderQualities(user.qualities)}</td>
+                                    <td>{user.profession.name}</td>
+                                    <td>{user.completedMeetings}</td>
+                                    <td>{getFormatRate(user)}</td>
+                                    <td><button className="btn btn-outline-danger" onClick={() => handleDelete(user._id)}>delete</button></td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            }
         </>
     );
 };
